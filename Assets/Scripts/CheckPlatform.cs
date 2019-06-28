b94c64f0e1e802a5d90c8965e193499d7b1bd175
LissetteHuilca <lissette.huilca@gmail.com>
2019-06-27 19:09:05 -0500

    Add script

---
+++ Assets/Scripts/CheckPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPlatform : MonoBehaviour
+{
+    private player play;
+    // Start is called before the first frame update
+    void Start()
+    {
+        play = GetComponentInParent<player>();
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+       
+        if (collision.gameObject.tag == "Plataforma")
+        {
+            play.transform.parent = collision.transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Plataforma")
+        {
+            play.transform.parent = null;
+        }
+    }
+}
